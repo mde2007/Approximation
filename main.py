f8e47298af3de89d5e47cfc2c90977460a90828a
Daniil <mai.moskovskiyde@gmail.com>
2026-09-21 17:46:18 +0300

    Метод Лагранжа

--- main.py
+++ main.py
@@ -5,22 +5,17 @@ import numpy as np
 def function(x):
     return x * (1 + x) ** (1 / 3)
 
-
-
 def tabulate(a = 1, b = 9, n = 8):
     step = (b - a) / n
     results = []
-    while a != b:
-        x, y = a, function(a)
-        results.append((a, function(a)))
-        a += step
+    for i in range(n + 1):            # <-- исправлено: цикл по счётчику
+        x = a + i * step
+        results.append((x, function(x)))
     return results
-
 
 graphic = tabulate()
 x_values = [point[0] for point in graphic]
 y_values = [point[1] for point in graphic]
-
 
 def mnk(x_values, y_values, m):
     n = len(x_values)
@@ -37,13 +32,11 @@ def mnk(x_values, y_values, m):
     coeffs = np.linalg.solve(ATA, ATY)
 
     func = f'{coeffs[0][0]:.4f}'
-    
     for i in range(1, len(coeffs)):
         func += f' + {coeffs[i][0]:.4f} * x ^ {i}'
     print(func)
 
-
-    global y_apr_value 
+    global y_apr_value
     y_apr_value = []
     apr_y = 0
     for x in x_values:
@@ -54,24 +47,25 @@ def mnk(x_values, y_values, m):
 
     print(y_apr_value)
 
+def lagrange_basis(x_values, i, x):
+    p = 1.0
+    for j in range(len(x_values)):
+        if i != j:
+            p *= (x - x_values[j]) / (x_values[i] - x_values[j])
+    return p
 
-def lagrange(x_valuse, y_values):
-    znam = x_values[1] - x_values[0]
-    shab = []
+def lagrange(x_values, y_values, x):
+    result = 0.0
     for i in range(len(x_values)):
-        p = 1
-        for j in range(len(x_values)):
-            if i != j:
-                p*= (x_values[i] - x_values[j])
-        shab.append(p)
-        p = 1
-    print(shab)
+        result += y_values[i] * lagrange_basis(x_values, i, x)
+    return result
 
 print(x_values, y_values)
-lagrange(x_values, y_values)
-# mnk(x_values, y_values, m = 3)
-# plt.plot(x_values, y_values, color = 'red')
-# print(x_values, y_apr_value)
-# plt.plot(x_values, y_apr_value, 'o--', color = 'blue' , markerfacecolor = 'green')
-# plt.grid(True)
-# plt.show()
+lagr_val = [lagrange(x_values, y_values, x) for x in x_values]
+mnk(x_values, y_values, m = 3)
+plt.plot(x_values, y_values, color='red', linestyle='-', linewidth=2, marker='o', label='f(x)')
+plt.plot(x_values, y_apr_value, color='blue', linestyle='--', linewidth=2, marker='s', label='МНК')
+plt.plot(x_values, lagr_val, color='green', linestyle=':', linewidth=2, marker='^', label='Лагранж')
+plt.legend()
+plt.grid(True)
+plt.show()
